5e038bbb321bf964d0a3d9f42c650dac91893d86
Juan K <Juan K@JuanK-PC>
2017-03-19 18:38:25 -0300

    Version 0.91

--- src/juank/BuscaMinas/logica/BuscaMinas.java
+++ src/juank/BuscaMinas/logica/BuscaMinas.java
@@ -1,5 +1,10 @@
 package juank.BuscaMinas.logica;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author Juan K
@@ -10,6 +15,19 @@ public class BuscaMinas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());//UI depende del sistema operativo
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(BuscaMinas.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(BuscaMinas.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(BuscaMinas.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(BuscaMinas.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         Juego juego;
         juego = new Juego();
         juego.iniciar();
