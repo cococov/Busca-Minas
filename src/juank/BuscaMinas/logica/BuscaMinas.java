4c7e7cc3c666dbed1674638c7d6aecc470ea9667
Juan K <Juan K@JuanK-PC>
2017-03-19 18:22:47 -0300

    Version 0.9

---
+++ src/juank/BuscaMinas/logica/BuscaMinas.java
@@ -0,0 +1,17 @@
+package juank.BuscaMinas.logica;
+
+/**
+ *
+ * @author Juan K
+ */
+public class BuscaMinas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Juego juego;
+        juego = new Juego();
+        juego.iniciar();
+    }
+}
